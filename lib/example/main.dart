8aecb085dbbd347b747c79c24c4cf3a4413c9d3c
ferdogan <fatih.erdogan@innology.com.tr>
2022-12-26 12:04:23 +0300

    necessary change

--- lib/example/main.dart
+++ lib/example/main.dart
@@ -2,7 +2,7 @@ import 'package:openai_dalle_wrapper/openai_dalle_wrapper.dart';
 
 void main() async {
   final openai = OpenaiDalleWrapper(
-    apiKey: "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",
+    apiKey: "apiKey",
   );
   final imagepath = await openai.generateImage("an astronout riding horse");
   print(imagepath);
