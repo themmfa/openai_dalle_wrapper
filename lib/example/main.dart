9d97357b668afeba3191b7f13f7999113d61e862
ferdogan <fatih.erdogan@innology.com.tr>
2022-12-26 12:05:57 +0300

    add edit image example

--- lib/example/main.dart
+++ lib/example/main.dart
@@ -8,4 +8,11 @@ void main() async {
   /// Generate image from text
   final imagepath = await openai.generateImage("an astronout riding horse");
   print(imagepath);
+
+  /// Edit an image with given text
+  final generatedImagesFromText = await openai.editImage(
+      "/Users/fatiherdogan/Desktop/mine/openai_dalle_wrapper/lib/assets/images/cute_dog.png",
+      "astronout dog",
+      1);
+  print(generatedImagesFromText);
 }
